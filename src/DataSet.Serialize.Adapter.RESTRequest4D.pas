f815dd85a0d16878228db6d3d9052ac65bf4dcce
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2023-05-30 15:40:13 -0300

    Big bang

---
+++ src/DataSet.Serialize.Adapter.RESTRequest4D.pas
@@ -0,0 +1,64 @@
+unit DataSet.Serialize.Adapter.RESTRequest4D;
+
+interface
+
+uses RESTRequest4D.Request.Adapter.Contract{$IFNDEF FPC}, DB{$ENDIF}, DataSet.Serialize;
+
+type
+  TDataSetSerializeAdapter = class(TInterfacedObject, IRequestAdapter)
+  private
+    FDataSet: TDataSet;
+    procedure Execute(const AContent: string);
+    {$IFNDEF FPC}
+    procedure ActiveCachedUpdates(const ADataSet: TDataSet; const AActive: Boolean);
+    {$ENDIF}
+    constructor Create(const ADataSet: TDataSet); reintroduce;
+  public
+    class function New(const ADataSet: TDataSet): IRequestAdapter;
+  end;
+
+implementation
+
+{$IFNDEF FPC}
+uses System.Generics.Collections, FireDAC.Comp.Client;
+
+procedure TDataSetSerializeAdapter.ActiveCachedUpdates(const ADataSet: TDataSet; const AActive: Boolean);
+var
+  LDataSet: TDataSet;
+  LDataSetDetails: TList<TDataSet>;
+begin
+  LDataSetDetails := TList<TDataSet>.Create;
+  try
+    if not AActive then
+      FDataSet.Close;
+    if ADataSet is TFDMemTable then
+      TFDMemTable(ADataSet).CachedUpdates := AActive;
+    if AActive and (not ADataSet.Active) and (ADataSet.FieldCount > 0) then
+      ADataSet.Open;
+    ADataSet.GetDetailDataSets(LDataSetDetails);
+    for LDataSet in LDataSetDetails do
+      ActiveCachedUpdates(LDataSet, AActive);
+  finally
+    LDataSetDetails.Free;
+  end;
+end;
+{$ENDIF}
+
+constructor TDataSetSerializeAdapter.Create(const ADataSet: TDataSet);
+begin
+  FDataSet := ADataSet;
+end;
+
+procedure TDataSetSerializeAdapter.Execute(const AContent: string);
+begin
+  ActiveCachedUpdates(FDataSet, False);
+  FDataSet.LoadFromJSON(aContent);
+  ActiveCachedUpdates(FDataSet, True);
+end;
+
+class function TDataSetSerializeAdapter.New(const ADataSet: TDataSet): IRequestAdapter;
+begin
+  Result := TDataSetSerializeAdapter.Create(ADataSet);
+end;
+
+end.
